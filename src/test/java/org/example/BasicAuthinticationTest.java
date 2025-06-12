17feb9c8b057bf6b20d8e98269baf57310ad353b
EmanAbusalim <emanaymanabusalim@gmail.com>
2025-06-12 15:30:45 +0300

    hit the Authrization server to get Acces token then parsing the response by json path then using the token to get request

---
+++ src/test/java/org/example/BasicAuthinticationTest.java
@@ -0,0 +1,35 @@
+package org.example;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.testng.annotations.Test;
+
+
+import static io.restassured.RestAssured.given;
+
+public class BasicAuthinticationTest {
+    @Test
+    public void getAccessToken() {
+   RestAssured.baseURI="https://rahulshettyacademy.com/oauthapi/oauth2/resourceOwner/token";
+      String stringResponse=  given().formParam("scope","trust")
+                .formParam("grant_type","client_credentials")
+                .formParam("client_secret","erZOWM9g3UtwNRj340YYaK_W")
+                .formParam("client_id","692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
+                .when().post().asString();
+        System.out.println(stringResponse);
+        JsonPath response = new JsonPath(stringResponse);
+        String accessToken=response.getString("access_token");
+//passing the Access token to another request
+        System.out.println(  given().queryParam("access_token",accessToken)
+                .when().get("https://rahulshettyacademy.com/oauthapi/getCourseDetails")
+                .asString());
+// parsing the
+    }
+
+
+
+
+
+
+
+
+}
